506dc24ca2674d976a4154937666308196d6a606
Jonathan Mouchou <jonmouchou@gmail.com>
2017-11-16 16:45:04 -0800

    Added comments
    Added comments to make code clearer

--- CryptoView/cryptoview.cpp
+++ CryptoView/cryptoview.cpp
@@ -23,12 +23,15 @@ void CryptoView::on_addCurrencyBtn_clicked()
 {
     ui->label_11->setText("Hello");
 
+    // Example usage of a get HTTP request
     // QUrl url("https://api.coinmarketcap.com/v1/ticker/?limit=5");
     QUrl url("https://api.coinmarketcap.com/v1/ticker/bitcoin/");
     QNetworkRequest request(url);
 
+    // Connect our finished sinal to the requestFinished slot
     connect(qnam, SIGNAL(finished(QNetworkReply*)), this, SLOT(requestFinished(QNetworkReply *)));
 
+    // Send the get request
     qnam->get(request);
 }
 
@@ -40,13 +43,17 @@ void CryptoView::LoadValues()
 
 void CryptoView::requestFinished(QNetworkReply *reply)
 {
+    // Read all data from the reply
     QString strReply = (QString)reply->readAll();
 
+    // Parse JSON
+    // Might be a better way of doing this
     QJsonDocument jsonDoc = QJsonDocument::fromJson(strReply.toUtf8());
     QJsonObject jsonObj = jsonDoc.object();
     QJsonArray jsonArr = jsonDoc.array();
     QJsonObject obj = jsonArr.first().toObject();
 
+    // Output JSON items in JSON object
     qDebug() << obj["name"].toString();
     qDebug() << obj["symbol"].toString();
     qDebug() << obj["price_usd"].toString();
